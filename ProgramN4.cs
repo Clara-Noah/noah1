b02cfb43dbf9720c94c7b9c498efc03c201da8fa
Clara-Noah <claraclaudio806@gmail.com>
2024-11-07 12:07:30 +0000

    Add files via upload

---
+++ ProgramN4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Project
+    {
+    public class N4
+    {
+        static void Main(string[] arg)
+        {
+            int x = 1;
+            int z = 2;
+            int w = 3;
+            int y = 1;
+
+            if(x == z && z == x && x == w && w == y) 
+            {
+                Console.WriteLine("os numeros são iguai");
+            }
+            else 
+            {
+                Console.WriteLine("os numeros não são iguais");
+            }
+        
+        }
+    }
+}
